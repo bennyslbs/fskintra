245c8de4a0111788762fff5364bdd3e8682cdf1b
Benny Simonsen (dovre) <benny@slbs.dk>
2015-03-06 22:30:52 +0100

    Generic extraction of lektier and formatting of output for Org-mode (web) and SMS

--- skoleintra/pgLektier.py
+++ skoleintra/pgLektier.py
@@ -7,6 +7,7 @@ import surllib
 import semail
 import urllib
 import re
+import datetime
 
 URL_PREFIX = 'http://%s/Infoweb/Fi/' % config.HOSTNAME
 URL_MAIN = URL_PREFIX + 'Dagbog/VisDagbog.asp?Periode=naestemaaned&'
@@ -34,7 +35,7 @@ def wpParseLektier(bs):
                     entr[k] = entr[k].renderContents().strip()
                     entr[k] = entr[k].replace("<div>", "").replace("</div>", "") # Remove <div>
                     entr[k] = entr[k].replace("&amp;", "&") # Replace some html escaping
-                    entr[k] = entr[k].replace('\xc2\xa0', '') # Special char, looks like blank
+                    entr[k] = entr[k].decode("UTF-8").strip()
                     if entr[k] == '':
                         entr[k] = None
                 if entr[0] != None and entr[1] != None:
@@ -66,14 +67,61 @@ def beautifyFagName(fag):
     else:
         return fag
 
-def wpFindLektier(bs):
-    title, lektier = wpParseLektier(bs)
+def shortWeekdayString(date):
+    weekday = date.isoweekday()
+    if weekday == 1:
+        return u'M'
+    if weekday == 2:
+        return u'Ti'
+    if weekday == 3:
+        return u'O'
+    if weekday == 4:
+        return u'To'
+    if weekday == 5:
+        return u'Fr'
+    if weekday == 6:
+        return u'L'
+    if weekday == 7:
+        return u'S'
+    else:
+        return u'?'
 
-    print '**', title
+def wpOrgPrintLektier(title, lektier):
+    res = '**' + title + "\n"
     for i in xrange(len(lektier) -1, 0, -1): # Rev-range since newest is the first on forældreintra
-        print "*** %s d. %4d.%02d.%02d" % (lektier[i]['weekday'], lektier[i]['year'], lektier[i]['month'], lektier[i]['day'])
+        res += "*** %s d. %4d.%02d.%02d\n" % (lektier[i]['weekday'], lektier[i]['year'], lektier[i]['month'], lektier[i]['day'])
         for fag, lektie in lektier[i]['lektier'].items():
-            print '- ' + beautifyFagName(fag) + ': ' + lektie
+            res += '- ' + beautifyFagName(fag) + ': ' + lektie + "\n"
+    return res
+
+def wpFormatSMSLektier(title, lektier, days):
+    res = ''
+    weekday = datetime.date.today().isoweekday()
+    if weekday == 5 and days < 7: # It's friday, add 2 days to days if <7
+        days += 2
+    today = datetime.date.today()
+    otherDate = datetime.date(2015, 12, 31)
+    # Tomorrow
+    for i in xrange(len(lektier) -1, 0, -1): # Rev-range since newest is the first on forældreintra
+        lektieDay = datetime.date(lektier[i]['year'], lektier[i]['month'], lektier[i]['day'])
+        delta = (lektieDay - today).days
+        if (delta >= 1 and delta <= days):
+            # Print date info, differs depending how many days from now
+            if delta == 1:
+                # Tomorrow - no date info
+                pass
+            elif delta < 7:
+                # Within next week: Just a short for the week day
+                res += shortWeekdayString(lektieDay) + ":\n"
+            else:
+                # Afer next week: A short for the week day + How many weeks, e.g. To1
+                res += shortWeekdayString(lektieDay) + str(int(delta/7)) + ":\n"
+
+            # Print lektie
+            for fag, lektie in lektier[i]['lektier'].items():
+                res += '' + beautifyFagName(fag) + ': ' + lektie + "\n"
+    res = res.rstrip() # Remove trailing \n
+    return res
 
 def skoleLektier(id):
     global bs
@@ -83,10 +131,16 @@ def skoleLektier(id):
 
     # read the initial page
     bs = surllib.skoleGetURL(URL_MAIN + "ID=%d" % id, True, True)
-    wpFindLektier(bs)
 
-def getLektieLister():
-    lektieIds = config.LEKTIEIDS
+    if True:
+        fh = open('/tmp/a.html', 'w')
+        fh.write(str(bs))
+        fh.close()
+    title, lektier = wpParseLektier(bs)
+    print wpOrgPrintLektier(title, lektier)
+    print wpFormatSMSLektier(title, lektier, 1)
+
+def getLektieLister(lektieIds):
     if len(lektieIds) == 0:
         config.log(u'Info: For at hente lektier: Adder linie i config filen: "lektieids=[1, 2, 3]"')
         config.log(u'Info- hvor tallene er ID=<num> for din(e) barn/børn i url\'en for Lektier')
@@ -95,7 +149,11 @@ def getLektieLister():
 
 if __name__ == '__main__':
     # test
-    #skoleLektier()
+    #skoleLektier(22)
 
     f = open('/tmp/a.html', 'r')
-    wpFindLektier(surllib.beautify(f.read()))
+    title, lektier = wpParseLektier(surllib.beautify(f.read()))
+    print "# Org-mode formatted content ###########################################"
+    print wpOrgPrintLektier(title, lektier)
+    print "# SMS content ##########################################################"
+    print wpFormatSMSLektier(title, lektier, 7)

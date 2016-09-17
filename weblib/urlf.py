836cec9b3611c6d66a7502551a8230b3f13d2856
Benny Simonsen <benny@slbs.dk>
2016-09-17 16:39:45 +0200

    urlf enhancement/fix
    If <meta refreresh ... > does not work, then try via javascript refresh
    If none of the refresh methods, then print a url and help to fix it (pure html)

--- weblib/urlf.py
+++ weblib/urlf.py
@@ -5,6 +5,7 @@ import sys
 import re
 import json
 import requests
+import os
 
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
@@ -37,5 +38,22 @@ def main(msg=''):
   <html lang="da">
   <meta charset="UTF-8">
   <meta http-equiv="refresh" content="0; url=%s">
+  <script type="text/javascript">
+    window.location = "%s";
+  </script>
+  <title>Link til %s</title>
+  <body>
+    <h2>Link til <a href="%s">%s</a></h2>
+
+    <h3>Hvorfor ser jeg denne side?</h3>
+    <p>Grunden til at du ser denne side, og ikke kommer videre er at din browser er sat til at ignorere %s.
+    <a href="%s">Søg på google for at slå det til i din browser</a>.
+    <br>
+    Desuden er javascript sandsynligvis også slået fra, ellers bude det også have sendt dig videre.
+    </p>
+  <body>
   </html>
-""" % u
+""" % (u, u, u, u, u,
+       cgi.escape('<meta http-equiv="refresh">'),
+       cgi.escape('https://www.google.dk/search?q=<meta http-equiv=refresh>+does+not+work+' + os.environ["HTTP_USER_AGENT"] + '"'),
+)
